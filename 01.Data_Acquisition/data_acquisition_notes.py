3476ee4960d01ba2653d2d9e5df1a86e26e8af05
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-11-18 19:35:04 +0200

    Add Data acquisition

---
+++ 01.Data_Acquisition/data_acquisition_notes.py
@@ -0,0 +1,25 @@
+"""
+1. Pandas library
+pd.DataFrame
+pd.Series
+pd.merge() - SQL Join
+
+2. Data sources:
+- CSV
+- Spreadsheets
+- APIs
+- Databases
+- Web scraping
+
+3. Data guidelines
+- Need to create a tabular structure from the raw data
+- Ensuring the data has been read without errors]
+- Manipulations to ensure the data is in a proper state (Tidy and clean)
+
+4. Merging data sources
+- Automating the process (Merge in pandas)
+- Document the process
+- Create as few datasets as possible
+- Ensure the different sources are compatible and consistent
+- Column type check
+"""
