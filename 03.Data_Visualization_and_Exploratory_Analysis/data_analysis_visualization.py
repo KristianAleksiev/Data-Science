0a4209e62bb7534dc8f42e12e09cc5bc09a3e544
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-11-20 16:44:00 +0200

    Add Data visualization, Exploratory analysis

---
+++ 03.Data_Visualization_and_Exploratory_Analysis/data_analysis_visualization.py
@@ -0,0 +1,25 @@
+"""
+1. Main concepts in Data Visualization
+- Human evolution and patterns spotting
+- Buffing the information of the data via plotting
+
+2. Types of graphs and purpose
+- Histograms - shows distributions
+- Boxplots - range and skewness of values
+- Bar charts - comparison of categories - plt.bar, plt.barh
+- Line plots - how one (dependent) variable varies with respect to an independent variable (time)
+- Pie charts - sizes between parts of a whole - plt.pie(data, labels=)
+- Single numbers - KPIs
+
+plt.style.available() -> Styling the plots
+
+3. Exploratory data analysis
+- What the data can tell us
+- Related to data cleaning
+
+4. Analytic graphs
+- Explaining the dataset visually - distributions, relations, comparisons
+- Show comparisons
+- Show causality
+- Show multivariable data
+"""
